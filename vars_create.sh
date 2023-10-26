d73992a8dc33003681e8273e0a56a5be2ffef39c
Jonathan Silva <52091910+jmpinheiros@users.noreply.github.com>
2023-10-26 10:39:23 -0300

    Update vars_create.sh

--- vars_create.sh
+++ vars_create.sh
@@ -2,8 +2,8 @@ export CLUSTER_NAME="cluster_name"
 export AWS_REGION="reg"
 export NODEGROUP_NAME="nodegroup_name"
 export NODE_TYPE="node_type"
-export N_NODES="nodes"
-export N_NODES_MIN="min_nodes"
-export N_NODES_MAX="max_nodes"
+export N_NODES=1
+export N_NODES_MIN=1
+export N_NODES_MAX=2
 
 # run with $ source ./vars_linux.sh
